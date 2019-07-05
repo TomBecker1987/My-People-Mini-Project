c98ef5dcf8782c62bd4cc5ab7afeb7dd7e906b88
Tom Becker <beckertomarch@gmail.com>
2019-07-05 11:41:04 +0300

    start project

---
+++ main.js
@@ -0,0 +1,35 @@
+//****************AJAX example****************//
+
+// let func = function (input) {
+//       $.ajax({
+//       method:"GET",
+//       url: `https://api.giphy.com/v1/gifs/search?api_key=QscPQqnoUkNdRdHOb0NU22NDjUXX0Mc6&q=${input}&limit=25&offset=0&rating=G&lang=en`,
+//       success: function(data) {
+//         // console.log(data)
+//         let firstGIF = data.data[0].embed_url
+//         $("body").append(`<iframe src="${firstGIF}" frameborder="0"></iframe>`)
+//         return firstGIF
+//       },
+//       error: function (xhr, text, error) {
+//         console.log(text)
+//       }
+//     })
+//   }
+//javascript, jQuery
+
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+// xhr.done(function(data) { console.log("success got data", data); });
+
+// func("bunny")
+
+
+
+//****************handlebars example****************//
+
+
+// let = animalsFunc = function () {
+//   let source = $("#animals-template").html()
+//   let template = Handlebars.compile(source)
+//   newHTML = template({animals})
+//   $(".animals").append(newHTML)
+// }
